d3441477483c39471cbe85e801cbdb432e112e09
Pritam Adhikari <pritam762.hitcsecs2020@gmail.com>
2024-01-15 22:45:50 +0530

    origin added

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.use(
     cors({
       origin: [
         "http://127.0.0.1:8000",
+        "https://photo-studio-backend.onrender.com"
       ],
       credentials: true,
       methods: "GET,POST,PUT,DELETE",
