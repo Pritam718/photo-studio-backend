3455eb0c62279794d90e6daa44de1c9d88412a6c
Pritam Adhikari <pritam762.hitcsecs2020@gmail.com>
2024-01-15 17:03:05 +0530

    db fix

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const router=require("./routes")
 const mongoose=require("mongoose");
 const cookieParser=require('cookie-parser')
 
+const PORT = process.env.PORT||8000
+const DB_URL=process.env.DB_URL||"mongodb://localhost:27017/Photo-Beckend";
 
-
-mongoose.connect("mongodb://localhost:27017/Photo-Beckend").then(()=>{
+mongoose.connect(DB_URL).then(()=>{
     console.log("db connected");
-    app.listen(8000,()=>{
+    app.listen(PORT,()=>{
         console.log('server is running')
     })
 }).catch((err)=>{
