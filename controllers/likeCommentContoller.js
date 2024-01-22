74c591f50c0674beea06fc002b6c585ea0ead192
Pritam Adhikari <pritam762.hitcsecs2020@gmail.com>
2024-01-22 11:55:13 +0530

    like count updated

--- controllers/likeCommentContoller.js
+++ controllers/likeCommentContoller.js
@@ -30,7 +30,7 @@ const getComment= async (req, res) => {
   
       const newLike=new likeModel({userId,postId,like});
       const savedLike = await newLike.save();
-      res.status(200).send({success:true,msg:"Comment submit successfully",data:savedLike});
+      res.status(200).send({success:true,msg:"Like submit successfully",data:savedLike});
 
     }catch(error){
       res.status(400).send({success:false,msg:error.message}); 
@@ -42,7 +42,7 @@ const getComment= async (req, res) => {
       const postId = req.params.postId;
       //const likes = await likeModel.find({ postId }).populate('userId');
       const likeCount = await likeModel.countDocuments({ postId });
-      console.log(likeCount)
+      //console.log(likeCount)
       // res.status(200).send({success:true,msg:"like submit successfully",data:likeCount});
       res.json({ likeCount });
     } catch (error) {
